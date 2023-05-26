1058c780dd6d4e79453a477bc6bf81450116f010
Chethini Kobewatta <ckavindya@gmail.com>
2023-05-26 09:24:53 +0530

    Edited the manage-items.html (2)

---
+++ front-end/js/manage-items.js
@@ -0,0 +1,7 @@
+const tbodyElm = $("#tbl-items tbody");
+const modalElm = $("#new-item-modal");
+const txtId = $("#txt-id");
+const txtName = $("#txt-name");
+const txtContact = $("#txt-contact");
+const txtAddress = $("#txt-address");
+const btnSave = $("#btn-save");
