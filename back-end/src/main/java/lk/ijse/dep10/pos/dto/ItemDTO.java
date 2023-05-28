f03f13ec3e88413f8e7afe88faaa5cd86f412569
Chethini Kobewatta <ckavindya@gmail.com>
2023-05-28 22:53:48 +0530

    created ItemDTO class

---
+++ back-end/src/main/java/lk/ijse/dep10/pos/dto/ItemDTO.java
@@ -0,0 +1,16 @@
+package lk.ijse.dep10.pos.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class ItemDTO {
+    private Integer id;
+    private String name;
+    private String address;
+
+
+}
